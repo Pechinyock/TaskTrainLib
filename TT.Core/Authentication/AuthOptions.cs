19bd3590e3be74b5d383396cb777d22f37cb9c70
Roslik Ivan <iroslik.dev@dent-it.ru>
2025-01-30 16:18:03 +0300

    auth common

---
+++ TT.Core/Authentication/AuthOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace TT.Core;
+
+public class AuthOptions
+{
+    public string Audience { get; set; }
+    public string Issuer { get; set; }
+    public string Key { get; set; }
+    public uint Lifetime { get; set; }
+}
---
+++ TT.Core/Authentication/AuthenticationDefaults.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TT.Core;
+
+public static class AuthenticationDefaults
+{
+    public static SymmetricSecurityKey GetSymmetricKey(string key) => new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+
+    public static TokenValidationParameters GetValidationParameters(AuthOptions options) 
+    {
+        ArgumentNullException.ThrowIfNull(nameof(options));
+        if (String.IsNullOrEmpty(options.Key))
+            throw new ArgumentNullException(nameof(options.Key));
+
+        var key = GetSymmetricKey(options.Key);
+
+        return new TokenValidationParameters() 
+        {
+            ValidateIssuer = true,
+            ValidIssuer = options.Issuer,
+
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = key,
+
+            ValidateAudience = true,
+            ValidAudience = options.Audience,
+
+            ValidateLifetime = true,
+        };
+    }
+
+    public static AuthOptions GetDefaultOptions() 
+    {
+        return new AuthOptions()
+        {
+            Issuer = "TaskTrain",
+            Audience = "TTClients",
+            Key = "mysupersecret_secretsecretsecretkey!123",
+            Lifetime = 5
+        };
+    }
+}
