8fa44610ac7fc49d0bc1cfe1998e77365de58b6a
Roslik Ivan <iroslik.dev@dent-it.ru>
2025-01-31 18:34:23 +0300

    add test project

---
+++ TT.Storage.Npgsql/NpgsqlDatabaseUpdater.cs
@@ -0,0 +1,52 @@
+﻿using Npgsql;
+using TT.Stroage;
+
+namespace TT.Storage.Npgsql;
+
+public class NpgsqlDatabaseUpdater : ISQLDatabaseUpdater
+{
+    private readonly string _connectionString;
+
+    public NpgsqlDatabaseUpdater(string connectionString)
+    {
+        if (String.IsNullOrEmpty(connectionString))
+            throw new ArgumentNullException(nameof(connectionString));
+
+        _connectionString = connectionString;
+    }
+
+    public void Downgrade(uint version)
+    {
+        throw new NotImplementedException();
+    }
+
+    public uint GetCurrentVersion()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> GetMigrationList()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetWorkingDatabaseName()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StepBack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StepForward()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(uint version)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Tests/TT.Storage.Npgsql.Tests/NpgsqlDatabaseUpdaterTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TT.Storage.Npgsql.Tests;
+
+public class NpgsqlDatabaseUpdaterTests
+{
+    private readonly string _connectionString = "Server=localhost;Port=11100;Database=postgres;Username=postgres;Password=qwerty12345;Pooling=true;Maximum Pool Size=10;Timeout=10;";
+
+    [Test]
+    public void TestDatabaseAccessable() 
+    {
+        var pgUpdater = new NpgsqlDatabaseUpdater(_connectionString);
+    }
+}
